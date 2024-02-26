28fe5b1f163c9b1cfc69f8d66c48dd01ec1c22a5
Sahara Guttridge <guttrids.gmatc.matc.edu>
2024-02-25 21:52:11 -0600

    Completed index.html

---
+++ activity-4/js/scripts.js
@@ -0,0 +1,27 @@
+
+var salutation = "WELCOME ";
+var user = "Mr. Hunsicker and classmates";
+var greeting = salutation + user;
+var greetingEl= document.getElementById('greeting');
+window.onload = loaded;
+function loaded()
+{
+    alert( greeting)
+}
+var price = 159.99;
+ var   studentDiscount = .20;
+ var   studentPrice = price - ( price * studentDiscount);
+ var  priceEl = document.getElementById('price');
+  var  studentPriceEl = document.getElementById('student-price');
+
+  priceEl.textContent = price.toFixed(2);
+  studentPriceEl.textContent = studentPrice.toFixed(2);
+
+  var price2 = 102.99;
+ 
+ var   studentPrice2 = price2 - ( price2 * studentDiscount);
+ var  price2El = document.getElementById('price2');
+  var  studentPrice2El = document.getElementById('student-price2');
+
+  price2El.textContent = price2.toFixed(2);
+  studentPrice2El.textContent = studentPrice2.toFixed(2);
